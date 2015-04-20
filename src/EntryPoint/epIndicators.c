cd4307f3256828e9b43d88089b704546031b3ec6
Mehmet Suyuti Dindar <mehmet.dindar@cardtek.com>
2015-04-20 16:14:45 +0300

    Entry Point Indicators yapisi ve temel fonksiyonlari eklendi.

---
+++ src/EntryPoint/epIndicators.c
@@ -0,0 +1,8 @@
+#include <string.h>
+
+#include "epIndicators.h"
+
+void epIndicators_reset(EpIndicatorsPtr epI)
+{
+    memset(epI, 0x00, sizeof(EpIndicators));
+}
---
+++ src/EntryPoint/epIndicators.h
@@ -0,0 +1,36 @@
+#ifndef _EPINDICATORS_H_
+#define _EPINDICATORS_H_
+
+typedef struct {
+    unsigned char statusCheckRequested;
+    unsigned char clessAppNotAllowed;
+    unsigned char zeroAmount;
+    unsigned char readerCvmReqLimitExceeded;
+    unsigned char readerClessFloorLimitExceeded;
+   // TTQ           copyOfTtq; 
+} EpIndicators, *EpIndicatorsPtr;
+
+void epIndicators_reset(EpIndicatorsPtr epI);
+
+
+
+#define SET_EPIND_STATUS_CHECK_REQ(ep)                      ((ep).statusCheckRequested          = 0x01)
+#define SET_EPIND_CLESS_APP_NOT_ALLOWED(ep)                 ((ep).clessAppNotAllowed            = 0x01)
+#define SET_EPIND_ZERO_AMOUNT(ep)                           ((ep).zeroAmount                    = 0x01)
+#define SET_EPIND_READER_CVM_REQ_LIMIT_EXCEEDED(ep)         ((ep).readerCvmReqLimitExceeded     = 0x01)
+#define SET_EPIND_READER_CLESS_FLOOR_LIMIT_EXCEEDED(ep)     ((ep).readerClessFloorLimitExceeded = 0x01)
+
+#define RESET_EPIND_STATUS_CHECK_REQ(ep)                    ((ep).statusCheckRequested          = 0x00)
+#define RESET_EPIND_CLESS_APP_NOT_ALLOWED(ep)               ((ep).clessAppNotAllowed            = 0x00)
+#define RESET_EPIND_ZERO_AMOUNT(ep)                         ((ep).zeroAmount                    = 0x00)
+#define RESET_EPIND_READER_CVM_REQ_LIMIT_EXCEEDED(ep)       ((ep).readerCvmReqLimitExceeded     = 0x00)
+#define RESET_EPIND_READER_CLESS_FLOOR_LIMIT_EXCEEDED(ep)   ((ep).readerClessFloorLimitExceeded = 0x00)
+
+#define IS_EPIND_STATUS_CHECK_REQ(ep)                       ((ep).statusCheckRequested          != 0x00)
+#define IS_EPIND_CLESS_APP_NOT_ALLOWED(ep)                  ((ep).clessAppNotAllowed            != 0x00)
+#define IS_EPIND_ZERO_AMOUNT(ep)                            ((ep).zeroAmount                    != 0x00)
+#define IS_EPIND_READER_CVM_REQ_LIMIT_EXCEEDED(ep)          ((ep).readerCvmReqLimitExceeded     != 0x00)
+#define IS_EPIND_READER_CLESS_FLOOR_LIMIT_EXCEEDED(ep)      ((ep).readerClessFloorLimitExceeded != 0x00)
+
+
+#endif// _EPINDICATORS_H_
