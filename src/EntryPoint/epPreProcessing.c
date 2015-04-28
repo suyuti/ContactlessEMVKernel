d39b8ff757fad5ce042f2e176c6b63da1e5d7eea
Mehmet Suyuti Dindar <mehmet.dindar@cardtek.com>
2015-04-28 12:54:50 +0300

    epIndicators will be reset at the begining of epPreProcessing

--- src/EntryPoint/epPreProcessing.c
+++ src/EntryPoint/epPreProcessing.c
@@ -1,9 +1,11 @@
 #include "epPreProcessing.h"
-
+#include "epTrnxParameters.h"
 #include "../Base/err.h"
 
 int epPreProcessing(void)
 {
+    epIndicators_reset();
+    //updateEpTrnxParameters(EpTrnxParamsPtr p, HalInterfacesPtr pHal);
     
     return SUCCESS;
 }
