9dc907a8c17e2cfe1fa15c96198f610e1bb27df9
Mehmet Suyuti Dindar <mehmet.dindar@cardtek.com>
2015-04-29 11:04:15 +0300

    general definitions added.

---
+++ src/Base/general.h
@@ -0,0 +1,14 @@
+#ifndef _GENERAL_H_
+#define _GENERAL_H_
+
+#define OK          1
+#define NOK         0
+#define SET         1
+#define RESET       0
+#define TRUE        1
+#define FALSE       0
+#define YES         1
+#define NO          0
+
+    
+#endif// _GENERAL_H_
