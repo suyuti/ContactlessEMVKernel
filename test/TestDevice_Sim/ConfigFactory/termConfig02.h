774b6d14fdc977993cd3d70764ec6b192307797e
Mehmet Suyuti Dindar <mehmet.dindar@cardtek.com>
2015-04-29 17:24:16 +0300

    generator methods converted into virtual to override interited classes.

--- test/TestDevice_Sim/ConfigFactory/termConfig02.h
+++ test/TestDevice_Sim/ConfigFactory/termConfig02.h
@@ -7,15 +7,15 @@ class TerminalConfig02 : public TerminalConfigs {
 public:
     TerminalConfigs generate(string kid, string aid);
 protected:
-    TerminalConfigs generateSet1();
-    TerminalConfigs generateSet2();
-    TerminalConfigs generateSet3();
-    TerminalConfigs generateSet4();
-    TerminalConfigs generateSet5();
-    TerminalConfigs generateSet6();
-    TerminalConfigs generateSet7();
-    TerminalConfigs generateSet8();
-    TerminalConfigs generateSet9();
+    virtual TerminalConfigs generateSet1();
+    virtual TerminalConfigs generateSet2();
+    virtual TerminalConfigs generateSet3();
+    virtual TerminalConfigs generateSet4();
+    virtual TerminalConfigs generateSet5();
+    virtual TerminalConfigs generateSet6();
+    virtual TerminalConfigs generateSet7();
+    virtual TerminalConfigs generateSet8();
+    virtual TerminalConfigs generateSet9();
 };
 
 #endif// _TERM_CONFIG_02_H_
