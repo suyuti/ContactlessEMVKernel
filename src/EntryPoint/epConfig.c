ae0ece2d129b462b8e51790f8ea3cba2359c1c00
Mehmet Suyuti Dindar <mehmet.dindar@cardtek.com>
2015-04-29 17:22:48 +0300

    epConfig reset method added.

--- src/EntryPoint/epConfig.c
+++ src/EntryPoint/epConfig.c
@@ -1,2 +1,10 @@
+#include <string.h> // memset
 #include "epConfig.h"
+#include "../Base/err.h"
 
+int resetEpConfig(EpConfigPtr p)
+{
+    if (!p) return INVALID_PARAMETER;
+    memset(p, 0x00, sizeof(EpConfig));
+    return SUCCESS;
+}
