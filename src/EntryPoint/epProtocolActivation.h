def71bd8875316251f73784f2acb5e91a07b07df
Mehmet Suyuti Dindar <mehmet.dindar@cardtek.com>
2015-06-24 09:39:02 +0300

    epProtocolActivation added.

---
+++ src/EntryPoint/epProtocolActivation.h
@@ -0,0 +1,15 @@
+#ifndef _EPPROTOCOLACTIVATION_H_
+#define _EPPROTOCOLACTIVATION_H_
+
+#include "./epCommon.h"
+
+int epProtocolActivation();
+
+int _3_2_1_1();
+int _3_2_1_2();
+int _3_2_1_3();
+int _3_2_1_4();
+int _3_2_1_5();
+int _3_2_1_6();
+
+#endif// _EPPROTOCOLACTIVATION_H_
